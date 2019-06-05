81abab224efbc79852c4a55298904382a1f32400
Nikita Kachko <Mykyta.Kachko@partner.bmwgroup.com>
2019-06-05 19:18:31 +0300

    Implemented moving average method

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ void buy(double fraction)
         ripples += minusAmount / values[currentIndex];
         money -= minusAmount;
         buyCount++;
-        cout << "Buy " << sellPrice << " " << values[currentIndex] << " money left " << money << endl;
+        // cout << "Buy " << sellPrice << " " << values[currentIndex] << " money left " << money << endl;
         buyPrice = values[currentIndex];
         sellPrice = 0;
     }
@@ -34,7 +34,7 @@ void sell(double fraction)
         money += minusAmount * values[currentIndex];
         ripples -= minusAmount;
         sellCount++;
-        cout << "Sell " << buyPrice << " " << values[currentIndex] << " ripples left " << ripples << endl;
+        // cout << "Sell " << buyPrice << " " << values[currentIndex] << " ripples left " << ripples << endl;
         sellPrice = values[currentIndex];
         buyPrice = INT_MAX;
     }
@@ -54,15 +54,15 @@ void readFromFile(string fileName)
     double num = 0.0, temp = 0.0;
     //keep storing values from the text file so long as data exists:
     while (ifile >> num) {
-        // values.insert(values.begin(), num);
-        values.push_back(num);
+        values.insert(values.begin(), num);
+        // values.push_back(num);
         ifile >> temp;
         ifile >> temp;
         ifile >> temp;
     }
 
     //verify that the scores were stored correctly:
-    for (int i = 0; i < values.size() / 10; ++i) {
+    for (int i = 0; i < 20; ++i) {
         cout << values[i] << endl;
     }
 }
@@ -113,34 +113,68 @@ auto simpleBuySell = [&] (const vector<double> &input, const int current) -> dou
     return 0;
 };
 
+auto movingAverage = [&] (const vector<double> &input, const int current, const int avgSpan) -> double
+{
+    // static const int avgSpan = 10;
+    double average = 0.0;
+    if (input.size() < 2 || input.size() < current - 1 || current < avgSpan)
+    return 0;
+
+    for (int i = current - avgSpan; i < current; i++)
+    {
+        average += input[i];
+    }
+    average /= avgSpan;
+
+    if (input[current] > average)
+    {
+        // cout << "Average is less, buying " << average << " " << input[current] << endl;
+        return 1;
+    }
+
+    if (input[current] < average)
+    {
+        // cout << "Average is more, selling " << average << " " << input[current] << endl;
+        return -1;
+    }
+
+    return 0;
+};
+
 int main()
 {
 
     readFromFile("data.txt");
 
-    currentIndex = 0;
-    while (currentIndex < values.size())
+    for (int i = 0; i < 50; i++)
     {
-        double fraction = simpleBuySell(values, currentIndex);
-        // cout << "Answer " << fraction << endl;
-        if (fraction > 0)
+        currentIndex = 0;
+        money = 500;
+        ripples = 0;
+        while (currentIndex < values.size())
         {
-            buy(fraction);
-        }
-        if (fraction < 0)
-        {
-            sell(fraction*(-1));
-        }
+            double fraction = movingAverage(values, currentIndex, i);
+            // cout << "Answer " << fraction << endl;
+            if (fraction > 0)
+            {
+                buy(fraction);
+            }
+            if (fraction < 0)
+            {
+                sell(fraction*(-1));
+            }
 
 
-        currentIndex++;
+            currentIndex++;
+        }
+        currentIndex--;
+        sell(1);
+        cout << "Size\t" << i << " USD " << money << endl;
+        // cout << "Selling at " << values[currentIndex] << endl;
     }
-    currentIndex--;
-    sell(1);
-    // cout << "Selling at " << values[currentIndex] << endl;
 
-    cout << "USD " << money << " and ripples " << ripples << endl;
-    cout << "Buy count " << buyCount << " sell count " << sellCount << endl;
+    // cout << "USD " << money << " and ripples " << ripples << endl;
+    // cout << "Buy count " << buyCount << " sell count " << sellCount << endl;
     // system("PAUSE");
     return 0;
 }
